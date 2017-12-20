9023f514177cd168caaab40e8f734d39be85e919
Justin K <justin-kim1@hotmail.com>
2017-12-20 16:49:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,8 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
   return array
 }
 function removeElementFromBeginningOfArray(array, element){
-  array = [1, 2, 3];
-  array.slice(1)
+  array = array.slice(1)
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array, element){
